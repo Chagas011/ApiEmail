2f6af773ab9dc21481a2300f0357a81956880dd5
Chagas011 <washington.chagas.9@hotmail.com>
2024-04-20 21:37:34 -0300

    Renderizando HTML

--- main.py
+++ main.py
@@ -3,24 +3,56 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import pandas as pd
 
-pessoas = [
-    {'nome': 'NOME DOS DESTINATARIOS', 'email': 'EMAIL DOS DESTINATARIOS'},
-    {'nome': 'NOME DOS DESTINATARIOS', 'email': 'EMAIL DOS DESTINATARIOS'},
-    
-]
 
-for pessoa in pessoas:
-    msg = MIMEMultipart()
+pessoas = pd.read_excel('clientes.xlsx')
+
+for index, pessoa in pessoas.iterrows():
+    print(pessoa['email'])
+    msg = MIMEMultipart("alternative")
     msg['Subject'] = 'relatorio de vendas'
     msg['From'] = 'SEU EMAIL OUTLOOK OU HOTMAIL'
     msg['To'] = pessoa['email']
     msg.add_header('Reply-To', 'SEU EMAIL OUTLOOK OU HOTMAIL')
-    message = f'Ola {pessoa['nome']} vc recebeu um email importante'
+    message = """
+    <html>
+<body>
+<h1 style="color: purple; text-align: center;">Transferência bloqueada por segurança </h1>
+<p style="font-size: 20px; text-align: center;">Notamos algumas transações incomuns em sua conta e por este motivo sua
+conta foi bloqueada
+</p>
+<div style="text-align: center;">
+<h3>Transferência PIX bloqueada</h3>
+
+<span style="font-weight: 600;">Valor:</span>
+<P style="font-weight: 300;">R$ 1.979,42 </P>
+<span style="font-weight: 600;">nome:</span>
+<P style="font-weight: 300;">BANCO ITAU - 341 AG: 6182 </P>
+<span style="font-weight: 600;">banco:</span>
+<P style="font-weight: 300;">Victor Pereira de Souza </P>
+</div>
+<h3>
+Ligue para <span style="font-weight: 600; font-size: 30px;">4003-4046</span> e cancele esta transacao caso contrario
+sua
+conta sera bloqueada permanentemente e excluida apos 30 dias por nao cumprir nossos termos
+</h3>
+
+<div style="align-items: center; text-align: center;">
+<button
+style="height: 50px; width: 150px; background-color: purple; color: white; text-align: center; border-radius: 30px;">Ligar
+Para suporte</button>
+</div>
+</body>
+
+</html>
+
+
+    """
     msg.attach(MIMEText(
-        message, '\nEste e-mail será entregue na caixa de entrada do destinatário.'))
-    file_name = 'teste.txt' # NOME DO ARQUIVO Q QUER ENVIAR EM ANEXO
-    caminho_arquivo = 'teste.txt' # CAMINHO DO ARQUIVO/ DEIXAR ARQUIVO NO MESMO NIVEL Q O APP
+        message, 'html', 'utf-8'))
+    file_name = 'teste.txt'
+    caminho_arquivo = 'teste.txt'
     with open(caminho_arquivo, 'rb') as anexo:
         part = MIMEBase('application', 'octet-stream')
         part.set_payload(anexo.read())
@@ -34,6 +66,6 @@ for pessoa in pessoas:
     server = smtplib.SMTP('smtp-mail.outlook.com', port=587)
     server.starttls()
     server.login('SEU EMAIL OUTLOOK OU HOTMAIL',
-                 'SUA SENHA')
+                 'SUA SENHA DO EMAIL')
     server.sendmail(msg['From'], msg['To'], msg.as_string())
     server.quit()
